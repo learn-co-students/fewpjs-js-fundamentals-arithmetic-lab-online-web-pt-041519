c85fd86ff5c18a13e9b6977bc9e369f0edecdf08
aparkening <aaron.parkening@gmail.com>
2019-11-14 12:14:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+// Multiply values num1 and num2 to equal 62
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+// Generate random integer greater than 0
+let random = Math.ceil(Math.random() * 10)
+
+// Generate remainder of 4
+let mod = 10 % 6
+
+// Return 20 as highest number in set
+let max = Math.max(12, 20, 1, 2, 4, 5, 10)
