7cdf518e542494a062559a35c77fa5c27387839e
Dan Sullivan <dansullivan19@gmail.com>
2019-11-16 17:51:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 31;
+let num2  = 2;
+let multiply = num1 * num2;
+
+let random  = Math.random();
+
+let mod = 40 % 9;
+
+let max  = Math.max(5, 20, 15, 19, 7, 10)
