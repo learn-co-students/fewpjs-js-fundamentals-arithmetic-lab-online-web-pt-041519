fe96db5bde316218cf8868237e3d5dcbb1c5688d
gzavaglia <gio.zavaglia@gmail.com>
2019-11-17 17:25:31 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
 // Write your code here
+const num1 = 7
+const num2 = 8.8
+const random = Math.floor(Math.random() * 10) + 1
+const mul = num1*num2
+const multiply = Math.round(mul)
+const mod = 19%5
+const max = Math.max(1, 2, 3, 4, 20);
