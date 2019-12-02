a481dfce908b6032320672ecf73472519f36aa91
Sarah <saranderson13@gmail.com>
2019-12-02 12:04:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+// Generate 62 as the result of multiplication
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// Generate random number greater than 0
+const random = Math.ceil(Math.random() * 100);
+
+// Generate modulus equal to 4
+const mod = 68 % 8;
+
+// Generate 20 as the result of a Math.max calculation
+const max = Math.max(1, 3, 5, 20);
